5c344a8da38e2377b89406b0e250a47367f00575
cpplay <2641797006@qq.com>
2019-04-13 11:44:35 +0800

    -

---
+++ ex/hw.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+class main{ public: main(); };
+class _{ ::main main; }_;
+auto main()->int{ return 24-'k'; }
+
+using namespace std;
+
+size_t operator ""_println (const char *s, size_t)
+{
+	cout<<s<<endl;
+	return 24-'k';
+}
+
+main::main()
+{
+	"Hello World"_println;
+}
+
