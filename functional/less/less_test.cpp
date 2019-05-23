6f3505bd64c5540c08f7ca957973dd542f0dbd90
cpplay <2641797006@qq.com>
2019-05-23 12:47:15 +0800

    -

---
+++ functional/less/less_test.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+
+template<typename _Arg1, typename _Arg2, typename _Result>
+struct binary_function
+{
+	typedef _Arg1 first_argument_type;
+	typedef _Arg2 second_argument_type;
+	typedef _Result result_type;
+};
+
+template<typename T = void>
+struct less;
+
+template<typename T>
+struct less : public binary_function<T, T, bool>
+{
+	constexpr
+	bool
+	operator()(const T& x, const T& y) const
+	{
+		return x < y;
+	}
+};
+
+template<>
+struct less<void>
+{
+	template <typename T, typename _Up>
+	constexpr
+	auto
+	operator()(T&& t, _Up&& u) const
+	noexcept(noexcept(std::forward<T>(t) < std::forward<_Up>(u)))
+	-> decltype(std::forward<T>(t) < std::forward<_Up>(u))
+	{
+		return std::forward<T>(t) < std::forward<_Up>(u);
+	}
+};
+
+
+template <typename A, typename B, typename U = less<>>
+bool f(A a, B b, U u = U())
+{
+    return u(a, b);
+}
+ 
+int main() 
+{
+    std::cout << std::boolalpha;   
+    std::cout << f(5, 20) << '\n';
+    std::cout << f(100, 10) << '\n';
+}
