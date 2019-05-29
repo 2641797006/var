1e4b48cdd77d9c46e3470309ab9c78c141610d0d
cpplay <2641797006@qq.com>
2019-05-29 10:57:29 +0800

    -

---
+++ c/unique_rand/f2/001.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define ResetCin()	while( fgetc(stdin) != '\n' ) {}
+
+int main()
+{
+	char *a;
+	int i, N, num, k;
+
+	srand(time(NULL));
+
+	printf("请输入随机数范围[1, N] N = ");
+	scanf("%d", &N);
+	ResetCin();
+	N = N<1 ? 1 : N;
+	a = malloc(N);
+	memset(a, 0, N);
+
+	printf("请输入随机数个数: ");
+	scanf("%d", &num);
+	ResetCin();
+	num = num<1 ? 1 : num;
+	num = num>N ? N : num;
+
+	for (i=0; i<num; ++i) {
+		k = rand()%N + 1;
+		if ( ! a[k-1] ) {
+			printf("%d ", k);
+			a[k-1] = 1;
+		}
+		else
+			--i;
+	}
+	puts("");
+}
